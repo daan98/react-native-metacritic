e5769ac52dc7c3985d828dda49b966e2666f5cf7
daan98 <daan98.da@gmail.com>
2025-06-25 21:43:54 -0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Button,
+  Image,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import icon from "./assets/icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Image
+        source={{
+          uri: `https://i.etsystatic.com/12175780/r/il/d29760/6147346614/il_1080xN.6147346614_40gh.jpg`,
+        }}
+        style={styles.iconImg}
+      />
+
+      <Text style={styles.text}>Bienvenido a la app</Text>
+
+      <Button
+        title="Pulsa aqui"
+        color={"red"}
+        onPress={() => {
+          alert("Hola!");
+        }}
+      /> */}
+
+      <TouchableHighlight
+        underlayColor={"blue"}
+        style={styles.touchableButton}
+        onPress={() => {
+          alert("Hola desde Touchable 🥳");
+        }}
+      >
+        <Text style={{ color: "white" }}>Pulsa bóton resaltable</Text>
+      </TouchableHighlight>
+
+      <TouchableOpacity
+        underlayColor={"blue"}
+        style={styles.touchableButton}
+        onPress={() => {
+          alert("Hola desde Touchable 🥳");
+        }}
+      >
+        <Text style={{ color: "white" }}>Pulsa bóton opacable</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +58,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 40,
+    color: "white",
+  },
+  iconImg: {
+    width: 400,
+    height: 400,
+    resizeMode: "contain",
+  },
+  touchableButton: {
+    width: 200,
+    height: 200,
+    backgroundColor: "red",
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 0,
+    marginVertical: 10,
   },
 });
